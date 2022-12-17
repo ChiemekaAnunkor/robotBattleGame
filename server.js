0c17c586583e3e8b058ba90ac464e7da5976773d
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-12-17 01:21:38 -0600

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const { bots, playerRecord } = require('./data')
 const { shuffleArray } = require('./utils')
 const Rollbar = require('rollbar');
 require("dotenv").config()
+var cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 app.use(express.static("public"))
