e1ff18ca78184c42f8afce1e9d51b033386cd64b
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-12-17 01:47:01 -0600

    added server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Rollbar = require('rollbar');
 const cors = require("cors")
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
